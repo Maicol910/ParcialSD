00b0994741831a7a8da94f398f5c87506ec34e20
maicol910 <maicoljosa910@gmail.com>
2022-10-20 18:43:34 -0500

    Actualizacion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,5 +5,5 @@ INSERT INTO tbl_grade (id, name) VALUES (4, 'Cuarto');
 INSERT INTO tbl_grade (id, name) VALUES (5, 'Quinto');
 INSERT INTO tbl_grade (id, name) VALUES (6, 'Sexto');
 
-INSERT INTO tbl_students (id,first_name,grade_id) VALUES(1,'Jose',1);
-INSERT INTO tbl_students (id,first_name,grade_id) VALUES(2,'Maicol',5);
+INSERT INTO tbl_students (id,first_name,grade_id) VALUES(1,'Maicol Jossa',1);
+INSERT INTO tbl_students (id,first_name,grade_id) VALUES(2,'Hugo Chaves',5);
