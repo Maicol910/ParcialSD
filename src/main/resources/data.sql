4f71090b67c2e98f4acd96bafe05898cf62c93ab
maicol910 <maicoljosa910@gmail.com>
2022-10-20 18:42:35 -0500

    Actualizacion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,9 @@
-INSERT INTO tbl_grade (id, name) VALUES (1, 'Sudamérica');
-INSERT INTO tbl_grade (id, name) VALUES (2, '2do de primaria');
-INSERT INTO tbl_grade (id, name) VALUES (3, 'Norteamérica');
-INSERT INTO tbl_grade (id, name) VALUES (4, 'Europa');
-INSERT INTO tbl_grade (id, name) VALUES (5, '8vo Semestre');
-INSERT INTO tbl_grade (id, name) VALUES (6, 'Africa');
-INSERT INTO tbl_grade (id, name) VALUES (7, 'Oceanía');
-INSERT INTO tbl_grade (id, name) VALUES (8, 'Antártida');
+INSERT INTO tbl_grade (id, name) VALUES (1, 'Primero');
+INSERT INTO tbl_grade (id, name) VALUES (2, 'Segundo');
+INSERT INTO tbl_grade (id, name) VALUES (3, 'Tercero');
+INSERT INTO tbl_grade (id, name) VALUES (4, 'Cuarto');
+INSERT INTO tbl_grade (id, name) VALUES (5, 'Quinto');
+INSERT INTO tbl_grade (id, name) VALUES (6, 'Sexto');
 
-INSERT INTO tbl_students (id,first_name,grade_id) VALUES(1,'Jose',5);
-INSERT INTO tbl_students (id,first_name,grade_id) VALUES(2,'Maicol',2);
+INSERT INTO tbl_students (id,first_name,grade_id) VALUES(1,'Jose',1);
+INSERT INTO tbl_students (id,first_name,grade_id) VALUES(2,'Maicol',5);
